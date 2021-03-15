caeba970e6ee7a253d3a71e171f56b97d46cb841
Yury <ymorpheus@gmail.com>
2021-03-15 20:33:27 +0300

    feat: implement Brackets task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  
+  if (str.length % 2 !== 0) {
+    return false
+  }
+
+  let stack = [],
+    openBrackets = '([{',
+    closeBrackets = ')]}';
+
+  let state = true
+
+  const strArr = str.split('')
+
+  strArr.forEach(el => {
+    if (openBrackets.includes(el)) {
+      let i = openBrackets.indexOf(el);
+      stack.push(closeBrackets[i]);
+    }
+    if (closeBrackets.includes(el) && el != stack.pop()) {
+      state = false
+    }
+  });
+
+  return state;
 }
